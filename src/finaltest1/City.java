b61a424637a9e131ce318417a84da590cf4d1df2
user <user@X550VC>
2015-06-20 09:42:53 +0800

    Greedy傳x和y值

---
+++ src/finaltest1/City.java
@@ -0,0 +1,36 @@
+package finaltest1;
+
+public class City {
+
+	private int x, y, id;
+
+	public int getID()
+	{
+		return id;
+	}
+	
+	public void setID(int id)
+	{
+		this.id = id;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+}
